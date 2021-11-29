99b913697dffe741ac6bea17c65c62a1d5c19fee
Rhythm Goel <rhythm.goel.17@gmail.com>
2021-11-29 01:23:25 +0000

    Readying for deployment

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 require('dotenv').config();
 const app = express();
 
+app.use(express.static('./client/build'));
+
 // Import routes from respective files
 const authRoutes = require('./routes/auth');
 const todoRoutes = require('./routes/todo');
@@ -14,6 +16,11 @@ app.use(express.urlencoded()); // allow url encoded data
 // Register routes
 app.use('/api/auth', authRoutes);
 app.use('/api/todo', todoRoutes);
+
+app.get("*", (req, res) => {
+   res.sendFile(path.resolve(__dirname, "client", "build",     
+   "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI;
