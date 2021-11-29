21c9729dd7cee77fc9b7f17222eb2ec09d3fb0e9
Rhythm Goel <rhythm.goel.17@gmail.com>
2021-11-29 04:23:36 +0000

    Fixed app server not running

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const todoRoutes = require('./routes/todo');
 
 // Initialize Middleware
 app.use(express.json()); // allow json data
-app.use(express.urlencoded()); // allow url encoded data
+app.use(express.urlencoded({ extended: true })); // allow url encoded data
 
 app.use(express.static('./client/build'));
 
@@ -26,13 +26,14 @@ app.get("*", (req, res) => {
 const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI;
 
-// mongoose.connect(`${MONGODB_URI}`)
-// .then(() => {
-//     console.log('MongoDB Connected...')
-//     app.listen(PORT, () => {
-//         console.log(`Server started on PORT ${PORT}`);
-//     });
-// })
-// .catch(err => {
-//     console.error(err.message);
-// })
+mongoose.connect(`${MONGODB_URI}`)
+.then(() => {
+    console.log('MongoDB Connected...')
+})
+.catch(err => {
+    console.error(err.message);
+})
+
+app.listen(PORT, () => {
+    console.log(`Server started on PORT ${PORT}`);
+});
